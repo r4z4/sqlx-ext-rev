7cd75250f1ae6c1280c84a9e7f3204758df4c572
r4z4 <r4z4aa@gmail.com>
2023-09-05 00:13:57 +0000

    delete route

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, App, HttpServer, http::header::ContentType, HttpResponse};
-use sqlx::postgres::{ PgPool, PgPoolOptions, PgRow };
+use sqlx::postgres::{ PgPool, PgPoolOptions, PgRow, PgQueryResult };
 use sqlx::*;
 use dotenvy_macro::dotenv;
+use serde::{Deserialize, Serialize};
 use std::env;
 
 #[derive(Clone)]
@@ -36,19 +37,42 @@ async fn root() -> String {
 async fn get_user(path: web::Path<usize>, app_state: web::Data::<AppState>) -> HttpResponse {
     let user_id: usize = path.into_inner();
 
-    #[derive(sqlx::FromRow)]
+    #[derive(Serialize, Deserialize)]
     struct User {
         user_id: i32,
         username: String,
     }
 
-    let user: sqlx::Result<Option<User>> = sqlx::query_as!(
+    // let user: sqlx::Result<Option<User>> = sqlx::query_as!(
+    //     User,
+    //     "SELECT user_id, username FROM users WHERE user_id = 1;"
+    // ).fetch_optional(&app_state.pool).await;
+
+    // HttpResponse::Ok()
+    //     .content_type(ContentType::plaintext())
+    //     .insert_header(("X-Hdr", "sample"))
+    //     .body("data")
+
+    let user: sqlx::Result<User> = sqlx::query_as!(
         User,
         "SELECT user_id, username FROM users WHERE user_id = 1;"
-    ).fetch_optional(&app_state.pool).await;
+    ).fetch_one(&app_state.pool).await;
 
-    HttpResponse::Ok()
-        .content_type(ContentType::plaintext())
-        .insert_header(("X-Hdr", "sample"))
-        .body("data")
+    match user {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(_) => HttpResponse::BadRequest().into(),
+    }
+}
+
+async fn delete_user(path: web::Path<usize>, app_state: web::Data<AppState>) -> HttpResponse {
+    let user_id: usize = path.into_inner();
+    let deleted: Result<PgQueryResult, sqlx::Error> = sqlx::query!(
+        "DELETE FROM users WHERE user_id = $1",
+        user_id as i32,
+    ).execute(&app_state.pool).await;
+
+    match deleted {
+        Ok(_) => HttpResponse::Ok().into(),
+        Err(_) => HttpResponse::BadRequest().into(),
+    }
 }
